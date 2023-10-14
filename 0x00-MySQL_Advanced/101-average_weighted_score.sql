e7be1fd1bffc12a38f722876d657fc4341bdbec1
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-14 05:09:11 -0700

    Task 13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,54 +1,29 @@
--- DROP PROCEDURE IF EXISTS statement is used to ensure that the procedure is dropped if it already exists.
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that
+-- computes and store the average weighted score for all students.
+-- Requirements:
+-- Procedure ComputeAverageWeightedScoreForUsers is not taking any input.
+-- Drop the procedure if it already exists.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 
--- Changing the delimiter to $$ to define the procedure body.
-DELIMITER $$
+-- Change the delimiter to define the procedure body.
+DELIMITER //
 
--- Creating a stored procedure named ComputeAverageWeightedScoreForUsers without any input parameters.
+-- Create the stored procedure without any input parameters.
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    -- Declare a local variable to store the user ID.
-    DECLARE user_id INT;
-
-    -- Declare a local variable to store the calculated weighted average score.
-    DECLARE w_avg_score FLOAT;
-
-    -- Declare a cursor to iterate through all users.
-    DECLARE user_cursor CURSOR FOR
-        SELECT id FROM users;
-
-    -- Initialize the cursor.
-    OPEN user_cursor;
-
-    -- Start looping through each user.
-    user_loop: LOOP
-        -- Fetch the next user ID from the cursor.
-        FETCH user_cursor INTO user_id;
-
-        -- Exit the loop when there are no more users.
-        IF user_id IS NULL THEN
-            LEAVE user_loop;
-        END IF;
-
-        -- Calculate the weighted average score and store it in w_avg_score.
-        SET w_avg_score = (
-            SELECT SUM(score * weight) / SUM(weight)
-            FROM users AS U
-            JOIN corrections as C ON U.id = C.user_id
-            JOIN projects AS P ON C.project_id = P.id
-            WHERE U.id = user_id
-        );
-
-        -- Update the users table, setting the average_score to the calculated w_avg_score for the current user.
-        UPDATE users SET average_score = w_avg_score WHERE id = user_id;
-
-    END LOOP;
-
-    -- Close the cursor.
-    CLOSE user_cursor;
+    -- Update the users table with the calculated weighted average scores.
+    UPDATE users AS U
+    JOIN (
+        SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
+        FROM users AS U
+        JOIN corrections AS C ON U.id = C.user_id
+        JOIN projects AS P ON C.project_id = P.id
+        GROUP BY U.id
+    ) AS WA ON U.id = WA.id
+    SET U.average_score = WA.w_avg;
 END
-$$
+//
 
--- Resetting the delimiter back to the default semicolon.
+-- Reset the delimiter back to the default semicolon.
 DELIMITER ;
 
