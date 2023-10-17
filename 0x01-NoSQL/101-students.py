2fc76388f0d4b0845ee7c6e99809260627cc03af
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-17 02:47:15 -0700

    Task 14

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 """ MongoDB Operations using pymongo """
 
+#!/usr/bin/env python3
+"""
+MongoDB Operations with Python using pymongo
+"""
 
 def top_students(mongo_collection):
     """
-    Get all students sorted by average score.
-
+    Returns all students sorted by average score.
     :param mongo_collection: PyMongo collection object
     :return: List of students with average score
     """
-    students = list(mongo_collection.find())
-    for student in students:
-        scores = student["scores"]
-        total_score = 0
-        count = 0
-        for score in scores:
-            total_score += score["score"]
-            count += 1
-        average_score = total_score / count if count > 0 else 0
-        student["averageScore"] = average_score
+    top = list(mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$scores.score"}
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ]))
 
-    sorted_students = sorted(students, 
-            key=lambda student: student["averageScore"], 
-            reverse=True)
-    return sorted_students
-
+    return top
